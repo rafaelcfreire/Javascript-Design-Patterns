006dc327208ab30cfa5ddc73dbb3ee7bb75b2bef
Rafael Freire <rafaelcfreire@gmail.com>
2015-02-20 17:38:09 -0200

    Revealing module pattern

--- Module_Pattern/revealingModule.js
+++ Module_Pattern/revealingModule.js
@@ -1,6 +1,27 @@
 var myRevealingModule = (function(){
+	var privateVar = "Ben Cherry",
+		publicVar = "Hey There!";
 
+    function privateFunction() {
+        console.log( "Name:" + privateVar );
+    }
+
+    function publicSetName( strName ) {
+        privateVar = strName;
+    }
+
+    function publicGetName() {
+        privateFunction();
+    }
+
+	// Reveal public pointers to
+    // private functions and properties
 	return {
-
+	    setName: publicSetName,
+	    greeting: publicVar,
+	    getName: publicGetName
 	};
 })();
+
+myRevealingModule.setName("Rafael");
+myRevealingModule.getName();
