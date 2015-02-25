837e93188e23b3c7253091df11bf1267a0b77dc3
Rafael Freire <rafaelcfreire@gmail.com>
2015-02-25 18:11:40 -0300

    Singleton implementation

--- Singleton_Pattern/singleton.js
+++ Singleton_Pattern/singleton.js
@@ -2,11 +2,39 @@ var mySingleton = (function(){
 	var instance;
 
 	function init(){
+		function privateMethod(){
+			console.log("I am a private");
+		}
+	
+	    var privateVariable = "Im also private";
+	 
+	    var privateRandomNumber = Math.random();
 
+	    return {
+		      // Public methods and variables
+		      publicMethod: function () {
+		        console.log( "The public can see me!" );
+		      },
+		 
+		      publicProperty: "I am also public",
+		 
+		      getRandomNumber: function() {
+		        return privateRandomNumber;
+		      }	    	
+	    };
 	};
 
 	return {
-
+	    // Get the Singleton instance if one exists
+	    // or create one if it doesn't
+	    getInstance: function () {
+	 
+	      if ( !instance ) {
+	        instance = init();
+	      }
+	 
+	      return instance;
+	    }
 	};
 })();
 
